ad83632d4bc99c23977616c7539720a7e069d363
Bence Mandi <mandi.bence@gmail.com>
2024-05-24 16:43:56 +0200

    modify the whole styling

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -2,12 +2,15 @@ import React from "react"
 import Header from "./components/Header"
 import Main from "./components/Main"
 import "./css/General.css"
+import "./css/App.css"
 
 const App: React.FC = () => {
     return (
-        <div>
-            <Header />
-            <Main />
+        <div className="App">
+            <div className="AppInnerContainer">
+                <Header />
+                <Main />
+            </div>
         </div>
     )
 }
